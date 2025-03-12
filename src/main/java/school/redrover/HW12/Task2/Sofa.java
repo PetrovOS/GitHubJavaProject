f8461016ab0887592c0ba883098d0b099cd6c565
PetrovOS <petrovos.ca@gmail.com>
2025-03-12 15:49:53 +0400

    Added final classes Chair, Sofa, Cabinet, Wardrobe

---
+++ src/main/java/school/redrover/HW12/Task2/Sofa.java
@@ -0,0 +1,9 @@
+package school.redrover.HW12.Task2;
+
+final class Sofa extends SeatingFurniture {
+
+    public Sofa(String material, String color, int seatingCapacity) {
+        super(material, color, seatingCapacity);
+    }
+
+}
---
+++ src/main/java/school/redrover/HW12/Task2/Wardrobe.java
@@ -0,0 +1,8 @@
+package school.redrover.HW12.Task2;
+
+final class Wardrobe extends StorageFurniture{
+
+    public Wardrobe (String material, String color, int storageCapacity) {
+        super(material, color, storageCapacity);
+    }
+}
